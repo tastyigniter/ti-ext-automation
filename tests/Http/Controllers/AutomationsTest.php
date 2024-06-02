02d0edeb31c540819201a5c34d53af9ed5292812
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2024-06-02 20:23:31 +0100

    Code refactor
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- tests/Http/Controllers/AutomationsTest.php
+++ tests/Http/Controllers/AutomationsTest.php
@@ -2,19 +2,18 @@
 
 namespace Igniter\Automation\Tests\Http\Controllers;
 
-use Igniter\Admin\Helpers\AdminHelper;
 use Igniter\Automation\AutomationRules\Actions\AssignToGroup;
 use Igniter\Automation\Classes\BaseCondition;
 use Igniter\Automation\Models\AutomationRule;
 use Igniter\User\Models\User;
 
-it('loads index page without errors', function() {
+it('loads automations page', function() {
     $this->actingAs(User::factory()->superUser()->create(), 'igniter-admin')
         ->get(route('igniter.automation.automations'))
         ->assertOk();
 });
 
-it('loads edit page without errors', function() {
+it('loads edit automation page', function() {
     AutomationRule::syncAll();
 
     $this->actingAs(User::factory()->superUser()->create(), 'igniter-admin')
@@ -22,16 +21,17 @@ it('loads edit page without errors', function() {
         ->assertOk();
 });
 
-it('loads create page without errors', function() {
+it('loads create automation page', function() {
     $this->actingAs(User::factory()->superUser()->create(), 'igniter-admin')
         ->get(route('igniter.automation.automations', ['slug' => 'create']))
         ->assertOk();
 });
 
-it('updates record without errors', function() {
+it('updates automation', function() {
     AutomationRule::syncAll();
 
-    $url = route('igniter.automation.automations', ['slug' => 'edit/'.AutomationRule::first()->getKey()]);
+    $automation = AutomationRule::first();
+    $url = route('igniter.automation.automations', ['slug' => 'edit/'.$automation->getKey()]);
     $this->actingAs(User::factory()->superUser()->create(), 'igniter-admin')
         ->post($url, [
             'Automation' => [
@@ -40,8 +40,23 @@ it('updates record without errors', function() {
         ], [
             'X-Requested-With' => 'XMLHttpRequest',
             'X-IGNITER-REQUEST-HANDLER' => 'onSave',
-        ])
-        ->assertJsonPath(AdminHelper::HANDLER_REDIRECT, $url);
+        ]);
+
+    expect(AutomationRule::find($automation->getKey()))->event_class->toBe('SomeClass');
+});
+
+it('deletes automation', function() {
+    AutomationRule::syncAll();
+
+    $automation = AutomationRule::first();
+    $url = route('igniter.automation.automations', ['slug' => 'edit/'.$automation->getKey()]);
+    $this->actingAs(User::factory()->superUser()->create(), 'igniter-admin')
+        ->post($url, [], [
+            'X-Requested-With' => 'XMLHttpRequest',
+            'X-IGNITER-REQUEST-HANDLER' => 'onDelete',
+        ]);
+
+    expect(AutomationRule::find($automation->getKey()))->toBeNull();
 });
 
 it('loads connector form field for', function($field, $className, $handler) {
